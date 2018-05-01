0a366121fdb8a71e304e01f5f4886e41fa5b39b6
Lawrence-Laptop <Lawrence.yip@stonybrook.edu>
2018-05-01 19:28:52 -0400

    Implemented masking with hand gestures

--- Assets/PlanetController.cs
+++ Assets/PlanetController.cs
@@ -9,12 +9,14 @@ public class PlanetController : MonoBehaviour {
     public Transform leftHand;
     public Transform rightHand;
     public Transform rotateTransform;
+    public Transform mask;
 
     public bool isScaleable;
     public bool isLeftHandTracking { get; set; }
     public bool isRightHandTracking { get; set; }
 
-
+    Vector3 avgForward;
+    Vector3 avgCross;
 
 // Use this for initialization
 void Start () {
@@ -24,21 +26,32 @@ void Start () {
 	// Update is called once per frame
 	void Update () {
         if (isLeftHandTracking && isRightHandTracking)
-            isScaleable = true;
-        else
-            isScaleable = false;
-
-        // if (isScaleable)
-        //UpdateRadius();
-
-        UpdateRotation();
+        {
+            UpdateRadius();
+            UpdateRotation();
+        }else if (isRightHandTracking)
+        {
+            UpdateMask();
+        }
 	}
 
     private void UpdateRotation()
     {
-        Vector3 angle = Vector3.Cross(leftHand.up, rightHand.up); 
-        Debug.Log(angle);
-        rotateTransform.localRotation = Quaternion.Euler(Vector3.Cross(leftHand.up, rightHand.up));
+        avgForward = leftHand.forward + rightHand.forward;
+        Vector3 avgUp = leftHand.up + rightHand.up;
+
+        avgCross = Vector3.Cross(Vector3.up, avgForward);
+
+        float crossMag = Vector3.Magnitude(Vector3.Cross(leftHand.up, rightHand.up));
+        //Debug.Log(crossMag);
+
+        //Vector3 clampedVector = avgForward.
+        float clampedY = Mathf.Clamp(avgForward.y, -1f, 0.8f);
+        avgForward.y = clampedY;
+
+        if (crossMag < 0.65)
+            rotateTransform.localRotation = Quaternion.LookRotation(avgForward);
+        Debug.Log("Avg Vector: " + avgForward.ToString() + "Rotation: " + rotateTransform.rotation);
     }
 
     public void UpdateRadius()
@@ -51,9 +64,28 @@ void Start () {
         float normalizedRadius = (handDist - 1.5f) / 2.5f;
         newPlanet.radius = normalizedRadius;
 
-
         float scaleFactor = normalizedRadius * 3 + 1;
         newPlanet.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
     }
 
+    private void UpdateMask()
+    {
+        if (isRightHandTracking && !isLeftHandTracking)
+        {
+            mask.gameObject.SetActive(true);
+            mask.transform.position = new Vector3(rightHand.transform.position.x, mask.transform.position.y, mask.transform.position.z);
+        }
+        else
+        {
+            mask.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 20), "Left Hand: " + leftHand.forward.ToString());
+        GUI.Label(new Rect(10, 30, 200, 20), "Right Hand: " + rightHand.forward.ToString());
+        GUI.Label(new Rect(10, 50, 200, 20), "Average: " + avgForward.ToString());
+        GUI.Label(new Rect(10, 70, 200, 20), "Cross: : " + avgCross.ToString());
+    }
 }
---
+++ Assets/Scripts/SetRenderQueue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRenderQueue : MonoBehaviour {
+    public int renderQueue = 3002;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.renderQueue = renderQueue;
+	}
+	
+}
