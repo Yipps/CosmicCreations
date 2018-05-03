4d39b56ebc7c0e357e4ffeae90e9a78835e0ac37
ektarana <ekta.rana.98@gmail.com>
2018-05-03 16:15:54 -0400

    updates

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -11,6 +11,9 @@ public class Orbit : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        //normalized the math stuff
+        //so that planets that are farther orbit slower
+        //then I randomized the angle from 20, 90 so that all the orbits dont happen at the same speed
         transform.RotateAround(Vector3.zero, Vector3.up, (((planetInfo.distFromSun - 0)/(10))*-1) * Time.deltaTime*Random.Range(20,90));
     }
 
--- Assets/Scripts/PlanetInfo.cs
+++ Assets/Scripts/PlanetInfo.cs
@@ -6,10 +6,12 @@ public class PlanetInfo : MonoBehaviour {
     public float hydrogen;
     public float helium;
     public float carbon;
-    public float iron;
     public float oxygen;
     public float neon;
     public float silicon;
+    public float iron;
+
+    public int[] elementArray = new int[7];
 
     public float radius;
     public float mass;
@@ -28,17 +30,56 @@ public class PlanetInfo : MonoBehaviour {
         distFromSun = DistFromSun();		
 	}
 
+    //makes the planet larger/smaller
     void Expand (float rad){
         gameObject.transform.localScale = new Vector3(rad, rad,rad);
     }
 
+    //orbits the planet around the origin
     void Rotate() {
         transform.Rotate(Vector3.right*Time.deltaTime*10); 
     }
 
+    //returns the distance from the Sun/origin as a vector quantity
     float DistFromSun() {
         float dist = transform.position.magnitude;
         return dist;
+    }
+
+    //ratio of metals to nm
+    void ProcessElements(){
+
+    }
+
+    //dynamically change element ratio
+    void AddElement(int planetIndex){
+
+        //hydrogen
+        if (elementArray[planetIndex] == 0) {
+              
+        }//helium
+        else if (elementArray[planetIndex] == 1){
+
+        }//carbon
+        else if (elementArray[planetIndex] == 2){
+
+        }//oxygen
+        else if (elementArray[planetIndex] == 3){
+
+        }//neon
+        else if (elementArray[planetIndex] == 4){
+
+        }//silicon
+        else if (elementArray[planetIndex] == 5){
+
+        }//iron
+        else if (elementArray[planetIndex] == 6){
+
+        }
+        else {
+            Debug.Log("this should be impossible");
+        }
+
     }
 
 }
