77ed8cbecc3869bc71284cc3eef3ef6da41c312a
ektarana <ekta.rana.98@gmail.com>
2018-05-08 13:25:26 -0400

    saved my scene with the trail renderings

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -11,7 +11,7 @@ public class Orbit : MonoBehaviour {
     void Start()
     {
         planetInfo = GetComponent<PlanetInfo>();
-        orbitSpeed = 45f;
+        orbitSpeed = 100f;
     }
 
     // Update is called once per frame
@@ -19,9 +19,6 @@ public class Orbit : MonoBehaviour {
     {
         //normalized the math stuff
         //so that planets that are farther orbit faster
-        //then I randomized the angle from 20, 90 so that all the orbits dont happen at the same speed
-        //transform.RotateAround(Vector3.zero, Vector3.up, ((planetInfo.distFromSun)/-1) * Time.deltaTime*4);
-
         curSpeed = orbitSpeed / Mathf.Sqrt(planetInfo.distFromSun);
         transform.RotateAround(Vector3.zero, Vector3.up, curSpeed * Time.deltaTime * 180 / (2 * Mathf.PI * planetInfo.distFromSun));
     }
