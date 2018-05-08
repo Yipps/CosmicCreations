bfd9979815ddde81c2a94e62e61081990db3ef01
ektarana <ekta.rana.98@gmail.com>
2018-05-07 23:24:44 -0400

    planet has a boring trail and now the outer planets move slower

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Orbit : MonoBehaviour {
-    PlanetInfo planetInfo; 
-    void Start () {
+    PlanetInfo planetInfo;
+
+    public float orbitSpeed;
+    public float curSpeed;
+
+    void Start()
+    {
         planetInfo = GetComponent<PlanetInfo>();
-	}
+        orbitSpeed = 45f;
+    }
 
     // Update is called once per frame
     void Update()
     {
         //normalized the math stuff
-        //so that planets that are farther orbit slower
+        //so that planets that are farther orbit faster
         //then I randomized the angle from 20, 90 so that all the orbits dont happen at the same speed
-        transform.RotateAround(Vector3.zero, Vector3.up, (((planetInfo.distFromSun - 0)/(10))*-1) * Time.deltaTime*Random.Range(20,90));
+        //transform.RotateAround(Vector3.zero, Vector3.up, ((planetInfo.distFromSun)/-1) * Time.deltaTime*4);
+
+        curSpeed = orbitSpeed / Mathf.Sqrt(planetInfo.distFromSun);
+        transform.RotateAround(Vector3.zero, Vector3.up, curSpeed * Time.deltaTime * 180 / (2 * Mathf.PI * planetInfo.distFromSun));
     }
 
 
